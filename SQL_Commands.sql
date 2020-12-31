2b451a1e58de746e418fa035e0272e903c0f0664
Ahmed AbdulAzeem <54190264+AhmadAbdulAzeem@users.noreply.github.com>
2020-12-31 18:06:56 +0200

    Update SQL_Commands.sql
    Data is inserted

--- SQL_Commands.sql
+++ SQL_Commands.sql
@@ -1,21 +1,24 @@
 CREATE TABLE Patient(
-	PID INTEGER NOT NULL,
+	SSN INTEGER NOT NULL,
 	Name VARCHAR(255),
+	Phone VARCHAR(255),
 	Gender VARCHAR(6),
+	Insuranceid  INTEGER,
 	Address varchar(255),
-	Date_Admitted DATE,
-	Date_Discharged DATE,
 	RID INTEGER,
-	PRIMARY KEY (PID),
+	PRIMARY KEY (SSN),
 	FOREIGN KEY (RID) REFERENCES Room (RID)
 	ON DELETE SET NULL
 	ON UPDATE CASCADE
 );
 
+
+
+
 CREATE TABLE Room(
 	RID INTEGER NOT NULL,
-	Room_Cost FLOAT,
-	No_of_Beds INTEGER,
+	Block_Floor INTEGER,
+	Unavailable VARCHAR(255),
 	PRIMARY KEY (RID)
 );
 
@@ -30,28 +33,34 @@ CREATE TABLE Room_Type(
 
 CREATE TABLE Medicine(
 	Code INTEGER NOT NULL,
+	Name VARCHAR(255),
+	Brand VARCHAR(255),
+	Description VARCHAR(255),
 	Price FLOAT,
-	Quality VARCHAR(255)
+	PRIMARY KEY (Code)
 );
 
 CREATE TABLE Takes(
 	PID INTEGER NOT NULL,
 	Code INTEGER NOT NULL,
-	Time TIME,
+	Time VARCHAR(255),
 	PRIMARY KEY (PID, Code),
 	FOREIGN KEY (Code) REFERENCES Medicine (Code)
+	ON DELETE SET NULL
+	ON UPDATE CASCADE,
+	FOREIGN KEY (PID) REFERENCES Patient (SSN)
 	ON DELETE SET NULL
 	ON UPDATE CASCADE
 );
 
 CREATE TABLE Employee(
-	EID INTEGER NOT NULL,
-	Salary INTEGER,
-	E_Address VARCHAR(255),
-	Gender VARCHAR(255),
+	SSN INTEGER NOT NULL,
 	E_Name VARCHAR(255),
 	Jop_Type VARCHAR(255),
-	PRIMARY KEY (EID)
+	Gender VARCHAR(255),
+	E_Address VARCHAR(255),
+	Salary INTEGER,
+	PRIMARY KEY (SSN)
 );
 
 CREATE TABLE E_Contact_Num(
@@ -65,7 +74,6 @@ CREATE TABLE E_Contact_Num(
 
 CREATE TABLE Doctor(
 	EID INTEGER NOT NULL,
-	Type VARCHAR(255),
 	Dep_ID INTEGER,
 	PRIMARY key (EID),
 	FOREIGN key (EID) REFERENCES Employee
@@ -86,7 +94,7 @@ CREATE TABLE Dep_Location(
 	Dep_ID INTEGER NOT NULL,
 	Location VARCHAR(255),
 	PRIMARY KEY (Dep_ID),
-	FOREIGN KEY (Dep_ID) REFERENCES Departmnt
+	FOREIGN KEY (Dep_ID) REFERENCES Departmnt (Dep_ID)
 	ON DELETE SET NULL
 	ON UPDATE CASCADE
 );
@@ -94,23 +102,7 @@ CREATE TABLE Dep_Location(
 CREATE TABLE Trainee(
 	EID INTEGER NOT NULL,
 	PRIMARY KEY (EID),
-	FOREIGN KEY (EID) REFERENCES Doctor (EID)
-	ON DELETE SET NULL
-	ON UPDATE CASCADE
-);
-
-CREATE TABLE Permanent(
-	EID INTEGER NOT NULL,
-	PRIMARY KEY (EID),
-	FOREIGN KEY (EID) REFERENCES Doctor (EID)
-	ON DELETE SET NULL
-	ON UPDATE CASCADE
-);
-
-CREATE TABLE Visitor(
-	EID INTEGER NOT NULL,
-	PRIMARY KEY (EID),
-	FOREIGN KEY (EID) REFERENCES Doctor (EID)
+	FOREIGN KEY (EID) REFERENCES Doctor (SSN)
 	ON DELETE SET NULL
 	ON UPDATE CASCADE
 );
@@ -131,8 +123,9 @@ CREATE TABLE Nurse(
 
 CREATE TABLE Receptionist(
 	EID INTEGER NOT NULL,
+	Shift_Hours INTEGER,
 	PRIMARY KEY (EID),
-	FOREIGN KEY (EID) REFERENCES Employee (EID)
+	FOREIGN KEY (EID) REFERENCES Employee (SSN)
 	ON DELETE SET NULL
 	ON UPDATE CASCADE
 );
@@ -146,6 +139,66 @@ CREATE TABLE Record(
 	PRIMARY KEY (RID),
 	FOREIGN KEY (EID) REFERENCES Receptionist (EID)
 	ON DELETE SET NULL
+	ON UPDATE CASCADE,
+	FOREIGN KEY (Patient_ID) REFERENCES Patient(SSN)
+	ON DELETE SET NULL
 	ON UPDATE CASCADE
 );
 
+
+INSERT INTO Room VALUES (1, 1, "Full");
+INSERT INTO Room VALUES (2, 4, "Full");
+INSERT INTO Room VALUES (3, 3, "Full");
+INSERT INTO Room VALUES (4, 1, "Full");
+INSERT INTO Room VALUES (5, 2, "Full");
+
+INSERT INTO Patient VALUES (100000001, "John Smith", "555-0256", "Male", 68476213 , "42 Foobar Lane", 1);
+INSERT INTO Patient VALUES (100000002, "Grace Ritchie", "555-0512", "Female", 36546321 , "37 Snafu Drive", 2);
+INSERT INTO Patient VALUES (100000003, "Random J. Patient", "555-1204", "Male", 65465421 , "101 Omgbbq Street", 3);
+INSERT INTO Patient VALUES (100000004, "Dennis Doe", "555-0256", "Female", 68421879 , "1100 Foobaz Avenue", 4);
+
+INSERT INTO Room_Type VALUES (1, "Single");
+INSERT INTO Room_Type VALUES (5, "Single");
+INSERT INTO Room_Type VALUES (3, "Double");
+INSERT INTO Room_Type VALUES (4, "Triple");
+
+INSERT INTO Medicine VALUES (1, "Procrastin-X", "X", "N/A", 10);
+INSERT INTO Medicine VALUES (2, "Thesisin", "Foo Labs", "A", 11.5);
+INSERT INTO Medicine VALUES (3, "Awakin", "Bar Laboratories", "A", 15.3);
+INSERT INTO Medicine VALUES (4, "Crescavitin", "Baz Industries", "A", 17.5);
+INSERT INTO Medicine VALUES (5, "Melioraurin", "Snafu Pharmaceuticals", "A", 24.1);
+
+INSERT INTO Takes VALUES (100000003, 1, "Every 6 hours");
+INSERT INTO Takes VALUES (100000004, 3, "Every 12 hours");
+INSERT INTO Takes VALUES (100000002, 2, "Every 8 hours");
+INSERT INTO Takes VALUES (100000001, 4, "Every 6 hours");
+
+INSERT INTO Employee VALUES (111111111, "John Dorian", "Doctor", "Male", "42 Foobar Lane", 1200);
+INSERT INTO Employee VALUES (333333333, "Christopher Turk", "Trainee", "Male", "37 Snafu Drive", 260);
+INSERT INTO Employee VALUES (888888888, "Keith Dudemeister", "Doctor", "Female", "101 Omgbbq Street", 1260);
+INSERT INTO Employee VALUES (999999999, "Molly Clock", "Nurse", "Female", "1100 Foobaz Avenue", 900);
+
+INSERT INTO E_Contact_Num VALUES(111111111, 6666-2314);
+INSERT INTO E_Contact_Num VALUES(111111111, 4632-1456);
+INSERT INTO E_Contact_Num VALUES(888888888, 5632-2159);
+INSERT INTO E_Contact_Num VALUES(999999999, 7777-2112);
+INSERT INTO E_Contact_Num VALUES(999999999, 3333-4444);
+INSERT INTO E_Contact_Num VALUES(333333333, 7878-6354);
+
+INSERT INTO Departmnt VALUES(1, "General Practitioner");
+INSERT INTO Departmnt VALUES(2, "Surgery");
+INSERT INTO Departmnt VALUES(3, "Critical-care");
+INSERT INTO Departmnt VALUES(4, "Padiatrist");
+
+INSERT INTO Dep_Location VALUES(1, "san francisco");
+INSERT INTO Dep_Location VALUES(2, "chicago");
+INSERT INTO Dep_Location VALUES(3, "chicago");
+INSERT INTO Dep_Location VALUES(4, "washington dc");
+
+INSERT INTO Doctor VALUES(111111111, 2);
+
+INSERT INTO Nurse VALUES (999999999, 1, 12, "Good");
+
+INSERT INTO Receptionist VALUES(888888888, 12);
+
+INSERT INTO Record VALUES(1, "Patient suffers from Headache", 100000001,  '2020-04-24 10:00:00', 888888888);
